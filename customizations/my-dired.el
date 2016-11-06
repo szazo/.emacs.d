44dcab8516e50f5bd9d5988ee89fef2acc125efe
Zoltan Szarvas <szazo@neting.hu>
2016-11-06 10:30:10 +0100

    enable dired omit mode

--- customizations/my-dired.el
+++ customizations/my-dired.el
@@ -1,2 +1,6 @@
  ;; If non-nil, Dired tries to guess a default target directory. This means: if there is a Dired buffer displayed in the next window, use its current directory, instead of this Dired buffer's current directory.
 (setq dired-dwim-target t)
+
+(require 'dired-x)
+(setq-default dired-omit-files-p t) ; Buffer-local variable
+;; (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
