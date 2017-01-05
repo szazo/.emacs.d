5a9addf0f3fc550da5a66c44c02f9263e8053161
Zoltan Szarvas <szazo@neting.hu>
2017-01-05 09:12:57 +0100

    wip

--- customizations/my-tabs.el
+++ customizations/my-tabs.el
@@ -2,10 +2,10 @@
 (setq-default c-basic-offset 2)
 (setq js-indent-level 2)
 (setq-default typescript-indent-level 2)
-(setq-default typescript-expr-indent-offset 2)
+(setq-default typescript-expr-indent-offset 4)
 (setq-default web-mode-code-indent-offset 2)
 (setq-default web-mode-markup-indent-offset 2)
-(setq-default web-mode-attr-indent-offset 2)
+(setq-default web-mode-attr-indent-offset 4)
 
 ;; enable mode which can detect indentation for the file and adapt settings to that
 (dtrt-indent-mode)
--- customizations/my-themes.el
+++ customizations/my-themes.el
@@ -1,1 +1,1 @@
-(load-theme 'zenburn t)
+(load-theme 'material t)
