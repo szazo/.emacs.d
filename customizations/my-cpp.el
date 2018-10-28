8aede7539e65bc500dc7058272d7de722f382d33
Zoltán Szarvas <zoltan.szarvas@neting.hu>
2018-10-28 14:15:11 +0100

    cleanup

--- customizations/my-cpp.el
+++ customizations/my-cpp.el
@@ -8,14 +8,3 @@
 ;; bind compiling with get-above-makefile to f5
 (global-set-key [f5] (lambda () (interactive) (compile (format
 	   "make -f %s" (get-above-makefile)))))
-
-;; company
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
-
-;; hook
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
