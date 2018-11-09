64c9a4a20cacca8c4ce4278f80ac6a6f57822602
Zoltán Szarvas <zoltan.szarvas@neting.hu>
2018-11-09 09:54:22 +0100

    disable irony for php

--- customizations/my-irony.el
+++ customizations/my-irony.el
@@ -1,36 +1,41 @@
-;; (req-package irony
-;;   :config
-;;   (progn
-;;     ;; If irony server was never installed, install it.
-;;     (unless (irony--find-server-executable) (call-interactively #'irony-install-server))
+(req-package irony
+  :config
+  (progn
+    ;; If irony server was never installed, install it.
+    (unless (irony--find-server-executable) (call-interactively #'irony-install-server))
 
-;;     (add-hook 'c++-mode-hook 'irony-mode)
-;;     (add-hook 'c-mode-hook 'irony-mode)
+    (defun my-irony-mode-on ()
+      ;; avoid enabling irony-mode in modes that inherits c-mode, e.g: php-mode
+      (when (member major-mode irony-supported-major-modes)
+        (irony-mode 1)))
 
-;;     ;; Use compilation database first, clang_complete as fallback.
-;;     (setq-default irony-cdb-compilation-databases '(irony-cdb-libclang
-;;                                                       irony-cdb-clang-complete))
+    (add-hook 'c++-mode-hook 'my-irony-mode)
+    (add-hook 'c-mode-hook 'my-irony-mode)
 
-;;     (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-;;   ))
+    ;; Use compilation database first, clang_complete as fallback.
+    (setq-default irony-cdb-compilation-databases '(irony-cdb-libclang
+                                                      irony-cdb-clang-complete))
 
-;;   ;; I use irony with company to get code completion.
-;;   (req-package company-irony
-;;     :require company irony
-;;     :config
-;;     (progn
-;;       (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))))
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  ))
 
-;;   ;; I use irony with flycheck to get real-time syntax checking.
-;;   (req-package flycheck-irony
-;;     :require flycheck irony
-;;     :config
-;;     (progn
-;;       (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))))
+  ;; I use irony with company to get code completion.
+  (req-package company-irony
+    :require company irony
+    :config
+    (progn
+      (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))))
 
-;;   ;; Eldoc shows argument list of the function you are currently writing in the echo area.
-;;   (req-package irony-eldoc
-;;     :require eldoc irony
-;;     :config
-;;     (progn
-;;       (add-hook 'irony-mode-hook #'irony-eldoc)))
+  ;; I use irony with flycheck to get real-time syntax checking.
+  (req-package flycheck-irony
+    :require flycheck irony
+    :config
+    (progn
+      (eval-after-load 'flycheck '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))))
+
+  ;; Eldoc shows argument list of the function you are currently writing in the echo area.
+  (req-package irony-eldoc
+    :require eldoc irony
+    :config
+    (progn
+      (add-hook 'irony-mode-hook #'irony-eldoc)))
