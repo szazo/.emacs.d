c87365fb8997a632dc8830150dc11ccd8e750b26
szazo <szazo@neting.hu>
2020-12-05 17:31:05 +0100

    use req package for ledger mode

--- customizations/my-ledger.el
+++ customizations/my-ledger.el
@@ -1,6 +1,5 @@
-(use-package ledger-mode
-	:ensure t
-	:init
-	(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
-	(setq ledger-post-auto-adjust-amounts 'true)
-	)
+(req-package ledger-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+  (setq ledger-post-auto-adjust-amounts 'true)
+  )
