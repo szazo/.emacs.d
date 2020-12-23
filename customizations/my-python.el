49bb82e81c90f9f604e877da899ea92eb74d33fd
szazo <szazo@neting.hu>
2020-12-23 09:34:51 +0100

    python jupyter notebooks

--- customizations/my-python.el
+++ customizations/my-python.el
@@ -3,4 +3,15 @@
   (elpy-enable)
   (setq elpy-rpc-python-command "python3")
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
+
+  ;; Use IPython for REPL
+  (setq python-shell-interpreter "jupyter"
+        python-shell-interpreter-args "console --simple-prompt"
+        python-shell-prompt-detect-failure-warning nil)
+  (add-to-list 'python-shell-completion-native-disabled-interpreters
+               "jupyter"))
+
+(req-package blacken)
+
+(req-package ein)
