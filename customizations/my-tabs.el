50c0c527476040fb1504a951651a94929c58fe16
Zoltán Szarvas <zoltan.szarvas@neting.hu>
2025-11-27 11:59:35 +0100

    feat: tab sizes

--- customizations/my-tabs.el
+++ customizations/my-tabs.el
@@ -1,13 +1,13 @@
 ;; disable tabs
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq-default c-basic-offset 2)
-(setq-default js-indent-level 2)
-(setq-default typescript-indent-level 2)
-(setq-default typescript-expr-indent-offset 4)
-(setq-default web-mode-code-indent-offset 2)
-(setq-default web-mode-markup-indent-offset 2)
-(setq-default web-mode-attr-indent-offset 2)
+(setq-default tab-width 4)
+(setq-default c-basic-offset 4)
+;; (setq-default js-indent-level 2)
+;; (setq-default typescript-indent-level 4)
+;; (setq-default typescript-expr-indent-offset 4)
+;; (setq-default web-mode-code-indent-offset 2)
+;; (setq-default web-mode-markup-indent-offset 2)
+;; (setq-default web-mode-attr-indent-offset 2)
 
 ;; enable mode which can detect indentation for the file and adapt settings to that
 (req-package dtrt-indent
--- customizations/my-typescript.el
+++ customizations/my-typescript.el
@@ -1,6 +1,8 @@
 ; https://www.ovistoica.com/blog/2024-7-05-modern-emacs-typescript-web-tsx-config
 ; https://notes.alexkehayias.com/setting-up-typescript-and-eslint-with-eglot/
 
+(setq-default typescript-ts-mode-indent-offset 4)
+
 (use-package eglot
   :ensure t
   :config 
